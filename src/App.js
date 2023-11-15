e3ad62b846df22835a1ea4093cef00300877993a
Catie Hart <cmh@anniecannons.com>
2023-11-15 09:38:45 -0800

    removed code

--- src/App.js
+++ src/App.js
@@ -7,31 +7,7 @@ import img4 from "./assets/weather-4.png";
 function App() {
 
   return (
-    <div className="card-wrapper">
-      <h1>Trending Today</h1>
-      <div className="card-container">
-        <div className="news-trends-card">
-          <img src={img1} />
-          <p>USA National Forecast</p>
-          <p>Looking Ahead to Your Halloween Forecast</p>
-        </div>
-        <div className="news-trends-card">
-          <img src={img2} />
-          <p>Wild Animals</p>
-          <p>'Lucky to be alive': Whale Takes Out Windsurfer</p>
-        </div>
-        <div className="news-trends-card">
-          <img src={img3} />
-          <p>Sponsored Content by Ford</p>
-          <p>5 Ways Driving At Night Could Be A Little Easier</p>
-        </div>
-        <div className="news-trends-card">
-          <img src={img4} />
-          <p>Weather News</p>
-          <p>Even Our Meteorologists Were Stumped At First</p>
-        </div>
-      </div>
-      <button>More News</button>
+   <div>
     </div>
   );
 }
